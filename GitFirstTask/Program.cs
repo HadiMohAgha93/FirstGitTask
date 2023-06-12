88f08a8f9b3fe22e19818551930f9eeaa2b660d8
Hadi Mohammed <hadi.m.agha@gmail.com>
2023-06-13 00:34:10 +0300

    first commit

---
+++ GitFirstTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFirstTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mr. Ali , Welcome to my GitHub");
+            Console.ReadLine();
+        }
+    }
+}
